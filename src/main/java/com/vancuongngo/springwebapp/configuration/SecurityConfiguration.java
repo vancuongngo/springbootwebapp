48efe398523abe6325bf0f935ad9f32e3649b076
Van Cuong Ngo <cuong.ngo@axonactive.vn>
2017-08-02 11:00:02 +0700

    - Applying remember-me using Simple hash-based token approach

--- src/main/java/com/vancuongngo/springwebapp/configuration/SecurityConfiguration.java
+++ src/main/java/com/vancuongngo/springwebapp/configuration/SecurityConfiguration.java
@@ -1,12 +1,7 @@
 package com.vancuongngo.springwebapp.configuration;
 
-import com.vancuongngo.springwebapp.service.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
+    /*@Autowired
     UserDetailsServiceImpl userDetailsService;
 
     @Autowired
@@ -32,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureAuthManager(AuthenticationManagerBuilder authenticationManagerBuilder){
         authenticationManagerBuilder.authenticationProvider(authenticationProvider);
-    }
+    }*/
 
     /* In order to access H2 DB console, we have to change 3 things in Spring Security, that are:
      * allow all requests to the H2 database console url ("/console/**")
@@ -45,13 +40,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/product/new", "/product/edit/*", "/product/delete/*").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                 .and()
+                .rememberMe().rememberMeParameter("remember-me-param").rememberMeCookieName("my-remember-me").tokenValiditySeconds(86400)
+                .and()
                 .logout().permitAll();
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
-     }
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("2").roles("ADMIN")
+                .and()
+                .withUser("user").password("1").roles("USER")
+        ;
+    }
 }
